f01480a8bdc9d83a9f31e99c3568f4393cf411f5
Mitchell Taylor <mtaylor22@neo.tamu.edu>
2014-07-29 22:17:52 -0500

    Cleaning up, added interactivity, packaging.

--- tagsphere.js
+++ tagsphere.js
@@ -1,19 +1,21 @@
 (function (window, undefined) {
-	var $tagsphere = function (size, BIGGEST_SIZE, SMALLEST_SIZE) {
+	var $tagsphere = function (holder, size, BIGGEST_SIZE, SMALLEST_SIZE, border) {
         if ( window === this ) {
-            return new $tagsphere(size, BIGGEST_SIZE, SMALLEST_SIZE);
+            return new $tagsphere(holder, size, BIGGEST_SIZE, SMALLEST_SIZE, border);
         }
+        if (border) $('#'+holder).css("border", "20px solid #ccc");
         this.BIGGEST_SIZE = BIGGEST_SIZE;
         this.SMALLEST_SIZE = SMALLEST_SIZE;
-        $tagsphere.prototype.x_speed = 0;
-		$tagsphere.prototype.y_speed = 0;
+        this.x_speed = 0;
+		this.y_speed = 0;
 		this.size = size;
-		$tagsphere.prototype.center = {'x': Math.floor(this.size/2), 'y': Math.floor(this.size/2)};
+		this.center = {'x': Math.floor((this.size)/2), 'y': Math.floor((this.size)/2)};
 		this.tags = [];
+		this.holder = holder;
 
 		var $this = this;
-		$(".tagsphere > li").each(function () {
-			var p = $tagsphere.prototype.position_to_point($tagsphere.prototype.random_position());
+		$('#'+holder+" .tagsphere > li").each(function () {
+			var p = $this.position_to_point($this.random_position());
 			var p = {};
 			p.lon = Math.random()*2*Math.PI;
 			p.lat = Math.random()*2*Math.PI;
@@ -24,43 +26,48 @@
 			p.tag = $(this).html();
 		    $this.tags.push(p);
 		});
-		$(".tagsphere").hide();
-		$('#tagsphere-holder').css({'background-color': '#DDD', 'width': this.size+'px', 'height': this.size+'px'});
+		$('#'+holder+" .tagsphere").hide();
+		$('#'+holder).css({'background-color': '#DDD', 'width': this.size+'px', 'height': this.size+'px'});
 		var max_width=0;
-		var center = $tagsphere.prototype.center;
+		var center = this.center;
 		this.tags.forEach(function (tag, i) {
-		    $('#tagsphere-holder').append('<span class="tag" id="tag-'+i+'">' + tag.tag + '</span>');
-		    $('#tag-'+i).css({'position': 'absolute', 'left': center.x+'px', 'top': center.y+'px', 'margin-left':tag.x+'px', 'margin-top': tag.y+'px'})
+		    $('#'+holder).append('<span class="tag" id="tag-'+i+'">' + tag.tag + '</span>');
+		    $('#'+holder+' #tag-'+i).css({'position': 'absolute', 'left': center.x+'px', 'top': center.y+'px', 'margin-left':tag.x+'px', 'margin-top': tag.y+'px'})
 		    .css({'font-size': BIGGEST_SIZE+'px'});
-			tag.width = $('#tag-'+i).width();
-			tag.height = $('#tag-'+i).height();
+			tag.width = $('#'+holder+' #tag-'+i).width();
+			tag.height = $('#'+holder+' #tag-'+i).height();
 			if (tag.width > max_width){
 				max_width = tag.width;
 			}
-			$('#tag-'+i + ' a').css({'color': "rgba(0,0,0,"+((tag.z+size/2)/(size*1.5))+")", 'font-size': ((BIGGEST_SIZE - SMALLEST_SIZE) *(tag.z+size/2)/(size) + SMALLEST_SIZE) +  "px"});
+			$('#'+holder+' #tag-'+i + ' a').css({'color': "rgba(0,0,0,"+((tag.z+size/2)/(size*1.5))+")", 'font-size': ((BIGGEST_SIZE - SMALLEST_SIZE) *(tag.z+size/2)/(size) + SMALLEST_SIZE) +  "px"});
 		});
-		$('#tagsphere-holder').css('padding', max_width/2);
-		$tagsphere.prototype.center.x+=max_width/2;
-		$tagsphere.prototype.center.y+=max_width/2;
-		$('.tag').css({'left': $tagsphere.prototype.center.x+'px', 'top': $tagsphere.prototype.center.y+'px'});
-
+		$('#'+holder).css('padding', max_width/2);
+		this.center.x+=max_width/2;
+		this.center.y+=max_width/2;
+		$('#'+holder+' .tag').css({'left': this.center.x+'px', 'top': this.center.y+'px'});
+		var $this=this;
 		$(document).mousemove(function(e){
-		    var $div = $("#tagsphere-holder");
+		    var $div = $('#'+holder);
 		    var div_x = e.pageX - $div.offset().left;
 		    var inside_x = (div_x  > 0 && div_x < $div.outerWidth());
 		    var div_y = e.pageY - $div.offset().top;
 		    var inside_y = (div_y  > 0 && div_y < $div.outerHeight());
 		    var inside = (inside_x && inside_y);
 		    if (inside){
-		    	$tagsphere.prototype.x_speed = (div_x - $tagsphere.prototype.center.x)/( $div.outerWidth()/2);
-		    	$tagsphere.prototype.y_speed = -1*(div_y - $tagsphere.prototype.center.y)/$div.outerHeight();
+		    	$this.x_speed = (div_x - $this.center.x)/( $div.outerWidth()/2);
+		    	$this.y_speed = -1*(div_y - $this.center.y)/$div.outerHeight();
 		    } else {
-		    	if (Math.abs($tagsphere.prototype.x_speed) > .1 || Math.abs($tagsphere.prototype.y_speed) > .1){
-		    		$tagsphere.prototype.y_speed/=2
-		    		$tagsphere.prototype.x_speed/=2;
+		    	if (Math.abs($this.x_speed) > .1 || Math.abs($this.y_speed) > .1){
+		    		$this.y_speed/=2
+		    		$this.x_speed/=2;
 		    	}
 		    }
 		});	
+		$('#'+holder+' .tag a').hover(function(){
+			$(this).css({'border': '2px dashed #777', 'text-shadow': '0 0 7px'  + $(this).css('color')});
+		},function(){
+			$(this).css({'border': 'none', 'text-shadow': 'none'});
+		});
         return this;
     };
     $tagsphere.fn = $tagsphere.prototype = {
@@ -72,7 +79,7 @@
 		return point;
 	},
     position_to_point: function(point){
-		rad = this.size/2;
+		var rad = this.size/2;
 	    point.x = rad * Math.cos(point.lon) * Math.cos(point.lat);
 	    point.y = rad * Math.sin(point.lat) * Math.sin(point.lon);
 	    point.z = rad * Math.sin(point.lat);
@@ -90,15 +97,17 @@
 			t.lon+=$this.y_speed/Math.PI; 
 			t.lon %= 2*Math.PI;
 			t = $this.position_to_point(t);
-			t.width = $('#tag-'+i).width();
-			t.height = $('#tag-'+i).height();
-			$('#tag-'+i).css({'position': 'absolute', 'left': $tagsphere.prototype.center.x-t.width/2+'px', 'top': $tagsphere.prototype.center.y-t.height/2+'px', 'margin-left':t.x+'px', 'margin-top': t.y+'px'} ,1000)
+			t.width = $('#'+$this.holder+' #tag-'+i).width();
+			t.height = $('#'+$this.holder+' #tag-'+i).height();
+			var newleft = $this.center.x-t.width/2;
+			var newtop = $this.center.y-t.height/2;
+			$('#'+$this.holder+' #tag-'+i).css({'left': newleft+'px', 'top': newtop+'px', 'margin-left':t.x+'px', 'margin-top': t.y+'px'})
 	        .css($this.distance_styling(t));
-	        $('#tag-'+i + ' a').css($this.distance_styling(t));
+	        $('#'+$this.holder+' #tag-'+i + ' a').css($this.distance_styling(t));
 		});
 	},
 	start: function(){
-		$this = this;
+		var $this = this;
 		setInterval(function(){$this.rotate($this)}, 100);
 		return this;
 	}
