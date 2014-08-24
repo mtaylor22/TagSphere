227f85ea357c3cb9d367496634b9a1b4bdc618df
Mitchell Taylor <mtaylor22@neo.tamu.edu>
2014-08-23 22:01:19 -0500

    Cleaning, adding index

--- flasker.py
+++ flasker.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 import os
 app = Flask('TagSphere')
 
-@app.route('/')
+@app.route('/demo')
 def demo():
 	return render_template('demo.html')
+@app.route('/')
+def demo():
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.debug = True
